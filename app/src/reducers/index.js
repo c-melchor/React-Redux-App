e4a5189e3d87d08836b79889fcf17b83816089c2
c_melchor <cah.melchor@gmail.com>
2020-12-09 11:33:58 -0800

    reducer index and store created

---
+++ app/src/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+  name: "",
+  breed: "",
+  image: "",
+  isFetching: false,
+  error: ""
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
