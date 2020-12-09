f5ad87a3eeb7ea36f6a35abf79fb7fa070d1d9eb
c_melchor <cah.melchor@gmail.com>
2020-12-09 11:50:42 -0800

    basic action template done

---
+++ app/src/actions/index.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const FETCHING_DOG_DATA = "FETCHING_DOG_DATA";
+export const DOG_DATA_SUCCESS = "DOG_DATA_SUCCESS";
+export const DOG_DATA_FAIL = "DOG_DATA_FAIL";
+
+export const getDogData = () => dispatch => {};
